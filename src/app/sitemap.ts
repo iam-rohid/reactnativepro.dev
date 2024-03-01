1f56196a5a3d65e4badcf03fc491acfc525d5ff9
Rohid <rohidul209@gmail.com>
2024-03-02 00:09:46 +0600

    sort posts in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,20 @@
 import type { MetadataRoute } from "next";
 import { allPosts, allAuthors, allTags } from "contentlayer/generated";
 import { SITE_URL } from "@/constants";
+import { compareDesc } from "date-fns";
 
-const addPathToBaseURL = (path: string) =>
-  path === "/" ? SITE_URL : `${SITE_URL}${path}`;
+const addPathToBaseURL = (path: string) => `${SITE_URL}${path}`;
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     "/",
     "/about",
     "/tutorials",
-    ...allPosts.map((item) => item.url),
     "/tags",
     ...allTags.map((item) => item.url),
+    ...allPosts
+      .sort((a, b) => compareDesc(a.publishedAt, b.publishedAt))
+      .map((item) => item.url),
     ...allAuthors.map((item) => item.url),
   ].map((route) => ({
     url: addPathToBaseURL(route),
